fec684322013eec4c1b3a3f8e03e79457a63087e
domieprangel <dominiquerangelperedo@gmail.com>
2025-08-15 02:22:18 -0600

    Agrega funciones agregarAmigo y limpiarCaja a app.js

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+const cont = 0;
+let amigos = [];
+/*Implementa una función para agregar amigos
+Desarrolla una función, que permita al usuario ingresar un nombre en el campo de texto y añadirlo a la lista de amigos creada anteriormente.
+
+Tareas específicas:
+
+Capturar el valor del campo de entrada: Utilizar document.getElementById o document.querySelector para obtener el texto ingresado por el usuario.
+
+Validar la entrada: Implementar una validación para asegurarse de que el campo no esté vacío. Si está vacío, mostrar un alert con un mensaje de error: "Por favor, inserte un nombre."
+
+Actualizar el array de amigos: Si el valor es válido, añadirlo al arreglo que almacena los nombre de amigos usando el método.push().
+
+Limpiar el campo de entrada: Después de añadir el nombre, restablecer el campo de texto a una cadena vacía.*/
+function agregarAmigo(){
+    let amigo = document.querySelector('#amigo').value;
+    if (amigo.trim() === "") alert("Por favor, inserte un nombre");       
+    else amigos.push(amigo);
+    limpiarCaja();
+}
+
+function limpiarCaja(){
+    document.querySelector('#amigo').value = "";
+}
