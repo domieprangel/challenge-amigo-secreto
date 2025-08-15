6999e65837a7e890cf128391e09f092d6dd594cf
domieprangel <dominiquerangelperedo@gmail.com>
2025-08-15 03:16:53 -0600

    Agerga función sortearAmigo a app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ Limpiar el campo de entrada: Después de añadir el nombre, restablecer el campo
 function agregarAmigo(){
     let amigo = document.querySelector('#amigo').value;
     if (amigo.trim() === "") alert("Por favor, inserte un nombre");       
-    else amigos.push(amigo);
+    else amigos.push(amigo); 
     limpiarCaja();
     mostrarAmigos();
 }
@@ -42,4 +42,20 @@ function mostrarAmigos(){
     const lista = document.querySelector('#listaAmigos');
     lista.innerHTML = "";
     amigos.forEach(amigo => {lista.innerHTML += `<li>${amigo}</li>`});
+}
+/*Escribe una función que seleccione de manera aleatoria uno de los nombres almacenados en el array amigos. Usa Math.random() y Math.floor() para obtener un índice aleatorio.
+
+Tareas específicas:
+
+Validar que haya amigos disponibles: Antes de sortear, comprobar si el array amigos no está vacío.
+
+Generar un índice aleatorio: Usar Math.random() y Math.floor() para seleccionar un índice aleatorio del arreglo.
+
+Obtener el nombre sorteado: Utilizar el índice aleatorio para acceder al nombre correspondiente en el arreglo.
+
+Mostrar el resultado: Actualizar el contenido del elemento de resultado utilizando document.getElementById()  e innerHTML para mostrar el amigo sorteado.*/
+function sortearAmigo(){
+    let iRandom = Math.floor(Math.random() * amigos.length);
+    console.log(amigos[iRandom])
+    document.getElementById('resultado').innerHTML += `<li>${amigos[iRandom]}</li>`;  
 }
