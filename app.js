8b3552f52825187cf556c238d8541d2553d124c2
domieprangel <dominiquerangelperedo@gmail.com>
2025-08-15 02:36:33 -0600

    Agrega función mostrarAmigos a app.js

--- app.js
+++ app.js
@@ -23,3 +23,25 @@ function agregarAmigo(){
 function limpiarCaja(){
     document.querySelector('#amigo').value = "";
 }
+
+function limpiarLista(){
+    document.querySelector('#listaAmigos') = "";
+}
+/*
+Crea una función que recorra el array amigos y agregue cada nombre como un elemento <li> dentro de una lista HTML. Usa innerHTML para limpiar la lista antes de agregar nuevos elementos.
+
+Tareas específicas:
+
+Obtener el elemento de la lista: Utilizar document.getElementById() o document.querySelector() para seleccionar la lista donde se mostrarán los amigos.
+
+Limpiar la lista existente: Establecer lista.innerHTML = "" para asegurarse de que no haya duplicados al actualizar.
+
+Iterar sobre el arreglo: Usa un bucle for para recorrer el arreglo amigos y crear elementos de lista (<li>) para cada título.
+
+Agregar elementos a la lista: Para cada amigo, crear un nuevo elemento de lista.
+ */
+function mostrarAmigos(){
+    const lista = document.querySelector('#listaAmigos');
+    lista.innerHTML = "";
+    amigos.forEach(amigo => {lista.innerHTML += `<li>${amigo}</li>`});
+}
