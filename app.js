996048bfea175db31dab97250a39297db1a46e12
domieprangel <dominiquerangelperedo@gmail.com>
2025-08-18 18:47:04 -0600

    Desarrolla función agregarAmigo

--- app.js
+++ app.js
@@ -8,3 +8,14 @@ function mostrarAmigos() {
     const lista = document.querySelector('#listaAmigos');
     lista.innerHTML = amigos.map(amigo => `<li>${amigo}</li>`).join('');
 }
+
+function agregarAmigo() {
+    let amigo = document.querySelector('#amigo').value.trim();
+    if(!amigo){
+        alert("Por favor, inserte un nombre.");
+        return
+    }
+    amigos.push(amigo);
+    limpiarCaja();
+    mostrarAmigos();
+}
