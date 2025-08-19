f3592b23a8b8ede5365bce1d2b8e458815665007
domieprangel <dominiquerangelperedo@gmail.com>
2025-08-18 18:36:18 -0600

    Desarrolla función mostrarAmigos

--- app.js
+++ app.js
@@ -3,3 +3,8 @@ let amigos = [];
 function limpiarCaja() {
     document.querySelector('#amigo').value = '';
 }
+
+function mostrarAmigos() {
+    const lista = document.querySelector('#listaAmigos');
+    lista.innerHTML = amigos.map(amigo => `<li>${amigo}</li>`).join('');
+}
